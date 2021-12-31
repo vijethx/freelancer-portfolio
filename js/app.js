2159ab3bd71acfc412daa4cf0edbc38bb511aeda
vijethx <38877972+vijethx@users.noreply.github.com>
2021-12-31 12:59:34 +0530

    added testimonials

--- js/app.js
+++ js/app.js
@@ -5,55 +5,71 @@ const records_wrap = document.querySelector(".records");
 const records_numbers = document.querySelectorAll(".number");
 
 filter_btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    filter_btns.forEach( (button) => button.classList.remove("active"));
-    btn.classList.add("active");
+	btn.addEventListener("click", () => {
+		filter_btns.forEach((button) => button.classList.remove("active"));
+		btn.classList.add("active");
 
-    let filterValue = btn.dataset.filter;
+		let filterValue = btn.dataset.filter;
 
-    $(".grid").isotope({filter: filterValue});
-  });
+		$(".grid").isotope({ filter: filterValue });
+	});
 });
 
-$('.grid').isotope({
-  itemSelector: '.grid-item',
-  layoutMode: 'fitRows',
-  transitionDuration:"0.6s",
+$(".grid").isotope({
+	itemSelector: ".grid-item",
+	layoutMode: "fitRows",
+	transitionDuration: "0.6s",
 });
 
 window.addEventListener("scroll", () => {
-  skillsEffect();
-  countUp();
-})
+	skillsEffect();
+	countUp();
+});
 
-function checkScroll(el){
-  let rect=el.getBoundingClientRect();
-  if(window.innerHeight >= rect.top + el.offsetHeight) return true;
-  return false;
+function checkScroll(el) {
+	let rect = el.getBoundingClientRect();
+	if (window.innerHeight >= rect.top + el.offsetHeight) return true;
+	return false;
 }
 
-function skillsEffect(){
-  if(!checkScroll(skills_wrap)) return;
-  skills_bars.forEach(skill => skill.style.width = skill.dataset.progress)
+function skillsEffect() {
+	if (!checkScroll(skills_wrap)) return;
+	skills_bars.forEach(
+		(skill) => (skill.style.width = skill.dataset.progress)
+	);
 }
 
-function countUp(){
-  if(!checkScroll(records_wrap)) return;
-  records_numbers.forEach(numb => {
-    const updateCount = () => {
-      let currentNum = +numb.innerText;
-      let maxNum = +numb.dataset.num;
-      let speed = 100;
-      const increment = Math.ceil(maxNum/speed);
+function countUp() {
+	if (!checkScroll(records_wrap)) return;
+	records_numbers.forEach((numb) => {
+		const updateCount = () => {
+			let currentNum = +numb.innerText;
+			let maxNum = +numb.dataset.num;
+			let speed = 100;
+			const increment = Math.ceil(maxNum / speed);
 
-      if(currentNum < maxNum) {
-        numb.innerText = currentNum + increment;
-        setTimeout(updateCount, 1);
-      } else {
-        numb.innerText = maxNum;
-      }
-    };
+			if (currentNum < maxNum) {
+				numb.innerText = currentNum + increment;
+				setTimeout(updateCount, 1);
+			} else {
+				numb.innerText = maxNum;
+			}
+		};
 
-    setTimeout(updateCount, 400);
-  })
+		setTimeout(updateCount, 400);
+	});
 }
+
+const swiper = new Swiper(".swiper-container", {
+  speed: 1100,
+  slidePerView:1,
+  loop:true,
+  autoplay:{
+    delay:5000,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
